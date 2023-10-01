5b6a6969ce8149f3ce3d5f14fa47c5a3564b8bf1
Pedro Moreira <pdcmoreira@gmail.com>
2023-10-01 01:59:02 +0100

    feat: add a transition between loading and content

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { useRouter, RouterView } from 'vue-router'
 import { useGithubOAuthFlow } from './composables/useGithubOAuthFlow'
 import { useUserStore } from './stores/user'
+import FadeTransition from '@/components/FadeTransition.vue'
 import LoadingSpinner from '@/components/LoadingSpinner.vue'
 import NavBar from './components/NavBar.vue'
 import InputButton from '@/components/InputButton.vue'
@@ -20,29 +21,31 @@ const logout = () => {
 </script>
 
 <template>
-  <div v-if="isLoading" class="flex h-full w-full flex-col items-center justify-center">
-    <LoadingSpinner size="xl" />
+  <FadeTransition>
+    <div v-if="isLoading" class="flex h-full w-full flex-col items-center justify-center">
+      <LoadingSpinner size="xl" />
 
-    <div class="mt-6 text-gray-500">Loading, please wait...</div>
-  </div>
+      <div class="mt-6 text-gray-500">Loading, please wait...</div>
+    </div>
 
-  <div v-else class="mx-auto p-4 sm:max-w-6xl sm:p-10">
-    <NavBar class="mb-4 sm:mb-10">
-      <div>
-        <InputButton v-if="!userStore.isLoggedIn" variant="primary" :href="authorizeUrl">
-          Log in with GitHub
-        </InputButton>
-
-        <template v-else>
-          <InputButton v-if="$route.name === 'home'" variant="primary" :to="{ name: 'gists' }">
-            Go to gists
+    <div v-else class="mx-auto p-4 sm:max-w-6xl sm:p-10">
+      <NavBar class="mb-4 sm:mb-10">
+        <div>
+          <InputButton v-if="!userStore.isLoggedIn" variant="primary" :href="authorizeUrl">
+            Log in with GitHub
           </InputButton>
 
-          <InputButton class="ml-2" @click="logout">Logout</InputButton>
-        </template>
-      </div>
-    </NavBar>
+          <template v-else>
+            <InputButton v-if="$route.name === 'home'" variant="primary" :to="{ name: 'gists' }">
+              Go to gists
+            </InputButton>
 
-    <RouterView />
-  </div>
+            <InputButton class="ml-2" @click="logout">Logout</InputButton>
+          </template>
+        </div>
+      </NavBar>
+
+      <RouterView />
+    </div>
+  </FadeTransition>
 </template>
---
+++ src/components/FadeTransition.vue
@@ -0,0 +1,11 @@
+<template>
+  <Transition
+    mode="out-in"
+    enter-from-class="opacity-0"
+    leave-to-class="opacity-0"
+    enter-active-class="transition duration-300"
+    leave-active-class="transition duration-300"
+  >
+    <slot />
+  </Transition>
+</template>
