f4f849641ebdb956b7a08c856eb5c9f68d7f0a44
Pedro Moreira <pdcmoreira@gmail.com>
2023-10-01 01:15:14 +0100

    fix: rendering content while loading

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@ const logout = () => {
   <!-- TODO: make a pretty loading -->
   <div v-if="isLoading">Loading...</div>
 
-  <div class="mx-auto p-4 sm:max-w-6xl sm:p-10">
+  <div v-else class="mx-auto p-4 sm:max-w-6xl sm:p-10">
     <NavBar class="mb-4 sm:mb-10">
       <div>
         <InputButton v-if="!userStore.isLoggedIn" variant="primary" :href="authorizeUrl">
