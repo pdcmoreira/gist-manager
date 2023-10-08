343a51acd1f400b96504eca0816aa0dfc5747770
Pedro Moreira <pdcmoreira@gmail.com>
2023-10-08 17:56:01 +0100

    feat: display total value

--- src/components/ChartTotals.vue
+++ src/components/ChartTotals.vue
@@ -1,13 +1,15 @@
 <script setup>
 import { computed } from 'vue'
 import { Pie } from 'vue-chartjs'
-import { Chart as ChartJS, PieController, ArcElement, Legend, Tooltip } from 'chart.js'
+import { Chart as ChartJS, PieController, ArcElement, Title, Legend, Tooltip } from 'chart.js'
 import { randomColor } from '@/utilities/randomColor'
 
 const props = defineProps({
   totals: {
     type: Array,
+
     required: true,
+
     validator: (totals) =>
       totals.every(
         (item) =>
@@ -19,23 +21,36 @@ const props = defineProps({
   }
 })
 
-ChartJS.register(PieController, ArcElement, Legend, Tooltip)
+ChartJS.register(PieController, ArcElement, Title, Legend, Tooltip)
 
-const options = {
+const labels = computed(() => props.totals.map((item) => item[0]))
+
+const values = computed(() => props.totals.map((item) => item[1]))
+
+const total = computed(() => values.value.reduce((sum, value) => sum + value, 0))
+
+const options = computed(() => ({
   responsive: true,
+
   plugins: {
+    title: {
+      display: true,
+      text: `Total: ${total.value}`,
+      position: 'bottom'
+    },
+
     legend: {
       position: 'bottom'
     }
   }
-}
+}))
 
 const data = computed(() => ({
-  labels: props.totals.map((item) => item[0]),
+  labels: labels.value,
 
   datasets: [
     {
-      data: props.totals.map((item) => item[1]),
+      data: values.value,
       backgroundColor: Array.from({ length: props.totals.length }, randomColor),
       hoverOffset: 4
     }
