63a2a522db0f416be6584560bdfa5c97defdf86a
Pedro Moreira <pdcmoreira@gmail.com>
2023-10-08 19:48:52 +0100

    fix: usage with "last-child" pseudo-class in slot content
    The corner actions container was a sibling, so it was the "last child" to the slot content.

--- src/components/ContainerCard.vue
+++ src/components/ContainerCard.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="relative rounded-lg border border-gray-100 bg-white shadow-md">
-    <slot />
+    <div>
+      <slot />
+    </div>
 
     <div class="absolute -right-3 -top-3">
       <slot name="corner-actions" />
