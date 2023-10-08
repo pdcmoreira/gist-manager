16956ae08bf2dcd5566e3324536e524b92475949
Pedro Moreira <pdcmoreira@gmail.com>
2023-10-08 18:22:40 +0100

    chore: remove unneeded color class

--- src/components/GistDetails.vue
+++ src/components/GistDetails.vue
@@ -14,7 +14,7 @@ const files = computed(() => (props.details ? Object.values(props.details.files)
 </script>
 
 <template>
-  <div class="p-6 pt-9 text-gray-700">
+  <div class="p-6 pt-9">
     <div class="mb-12 flex items-end justify-between border-b px-4 pb-6">
       <div :class="{ italic: !details.description }">
         {{ details.description || 'No description' }}
@@ -26,7 +26,7 @@ const files = computed(() => (props.details ? Object.values(props.details.files)
     </div>
 
     <div v-for="file in files" :key="file.filename" class="mb-12 last:mb-0">
-      <div class="mb-2 flex items-end justify-between px-4 text-gray-700">
+      <div class="mb-2 flex items-end justify-between px-4">
         <div class="font-mono text-sm">
           {{ file.filename }}
         </div>
